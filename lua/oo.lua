56f78b937f98ad12234f001c69985536aa2296f0
liuhongming <liuhongming@senyint.com>
2017-06-29 15:50:02 +0800

    @fix lua 元方法、面向对象

--- lua/oo.lua
+++ lua/oo.lua
@@ -1,7 +1,9 @@
+
 local _M = { _VERSION = '0.08' }
+--元方法
 local mt = { __index = _M }
 
-local mytable = setmetatable({}, mt)
+local mytable = setmetatable({}, mt) --重载函数
 
 print(mytable._VERSION)
 
